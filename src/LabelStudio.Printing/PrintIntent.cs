5991d5bfacc64b7af5c87c6fbbf806b0cd4c3534
TresPontos <jose.eduardo.correia.viana@gmail.com>
2026-09-18 19:15:58 +0100

    Scaffold Editor/Desktop projects, add SkiaSharp, remove broken PrintIntent.CreateRenderTarget

--- src/LabelStudio.Printing/PrintIntent.cs
+++ src/LabelStudio.Printing/PrintIntent.cs
@@ -1,6 +1,5 @@
 using LabelStudio.Document;
 using LabelStudio.Layout;
-using LabelStudio.Rendering;
 
 namespace LabelStudio.Printing;
 
@@ -8,26 +7,4 @@ public sealed record PrintIntent(
     DocumentId DocumentId,
     PreparedScene Scene,
     MediaProfile Media,
-    PrintSettings Settings)
-{
-    public RenderTarget CreateRenderTarget() =>
-        new(
-            Settings.Dpi,
-            Settings.Dpi,
-            0,
-            0,
-            Document.Units.MicrometreRect.Zero,
-            0,
-            0,
-            CreateInkChannels(Media));
-
-    private static IReadOnlyList<InkOutputChannel> CreateInkChannels(MediaProfile media)
-    {
-        List<InkOutputChannel> channels = [new("Black", false)];
-        if (media.SupportsRed)
-        {
-            channels.Add(new("Red", true));
-        }
-        return channels;
-    }
-}
+    PrintSettings Settings);
